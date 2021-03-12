7acc2baf04297b6d786f5e1e663f9b86eb357066
shaik <hshaik@live.unc.edu>
2021-03-12 07:07:17 -0500

    fixed itt!!

---
+++ main.js
@@ -0,0 +1,25 @@
+
+let current_tag = ""; 
+
+AFRAME.registerComponent('registerevents', {
+    init: function () {
+        const marker = this.el;
+
+        marker.addEventListener("markerFound", ()=> {
+            current_tag = "lala"; 
+        });
+
+        marker.addEventListener("markerLost",() =>{
+            console.log('Marker Lost: ');
+        });
+    },
+});
+
+
+function decrypt() {
+
+    let input = document.getElementById("decryptxt").value;
+
+    document.getElementById("im1").setAttribute("value", current_tag+input); 
+ 
+}
