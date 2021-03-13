5b1dabba88b13ebebc22bded9a5f31766c5dc73c
shaik <hshaik@live.unc.edu>
2021-03-13 14:48:19 -0500

    Add code

--- main.js
+++ main.js
@@ -29,6 +29,7 @@ function decrypt() {
 
     console.log("I am here");
     let inputVar = document.getElementById("secret").value; 
+    console.log(inputVar); 
     let cleanInput = inputVar.replace(/\s+/g, '');
     let decode = cypher(current_cypher, cleanInput, true); 
     console.log(decode);   
